8b665685c8f41c3ff1bc4401f3b2804da3f2e1c7
miku-wwl <569144003@qq.com>
2024-10-09 00:36:20 +0800

    feat:Import shadcn ui button

--- images_frontend/src/app/page.tsx
+++ images_frontend/src/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/Button";
+
 export default function Home() {
-  return <div>Root Page</div>;
+  return (
+      <div>
+        Root Page{" "}
+        <Button variant="ghost" type="submit">
+          Click Me
+        </Button>
+      </div>
+  );
 }
