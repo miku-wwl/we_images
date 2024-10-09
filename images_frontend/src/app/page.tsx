b0d0d1b2656f197d8d97c1d2a6cb113d28ce097f
miku-wwl <569144003@qq.com>
2024-10-09 23:27:08 +0800

    feat:Input.tsx & Textarea.tsx

--- images_frontend/src/app/page.tsx
+++ images_frontend/src/app/page.tsx
@@ -1,12 +1,19 @@
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
+import { Textarea } from "@/components/Textarea";
 
 export default function Home() {
-  return (
-      <div>
-        Root Page{" "}
-        <Button variant="ghost" type="submit">
-          Click Me
-        </Button>
-      </div>
-  );
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <form className="w-full max-w-md flex flex-col gap-4">
+                <h1 className="text-center text-2xl font-bold">Create App</h1>
+                <Input name="name" placeholder="App Name"></Input>
+                <Textarea
+                    name="description"
+                    placeholder="Description"
+                ></Textarea>
+                <Button type="submit">Submit</Button>
+            </form>
+        </div>
+    );
 }
