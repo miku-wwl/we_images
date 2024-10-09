36c4cd84b4c78dee3f50b46c0f791a674d7fa226
miku-wwl <569144003@qq.com>
2024-10-10 00:35:58 +0800

    feat:init Spring data jpa & swagger & user table query

---
+++ images_backend/src/main/java/com/weilai/images/entity/User.java
@@ -0,0 +1,20 @@
+package com.weilai.images.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    private String name;
+}
---
+++ images_backend/src/main/java/com/weilai/images/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.weilai.images.repository;
+
+import com.weilai.images.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
+}
