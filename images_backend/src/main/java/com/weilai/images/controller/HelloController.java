8705a770ab0eb31549f35d3ae4acaa946cee1839
miku-wwl <569144003@qq.com>
2024-10-10 22:13:53 +0800

    feat:Import Oauth2

--- images_backend/src/main/java/com/weilai/images/controller/HelloController.java
+++ images_backend/src/main/java/com/weilai/images/controller/HelloController.java
@@ -1,15 +1,26 @@
 package com.weilai.images.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/hello")
-public class HelloController {
+import java.util.HashMap;
 
-    @GetMapping("/index")
-    public String Index() {
-        return "Index";
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public ResponseEntity<?> index(
+            @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+            @AuthenticationPrincipal OAuth2User oauth2User) {
+        var userInfo = new HashMap<String, Object>();
+        userInfo.put("userName", oauth2User.getName());
+        userInfo.put("clientName", authorizedClient.getClientRegistration().getClientName());
+        userInfo.put("userAttributes", oauth2User.getAttributes());
+        return ResponseEntity.ok().body(userInfo);
     }
 }
